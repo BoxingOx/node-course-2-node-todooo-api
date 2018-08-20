deb67f0b7597f07203a5618a93d0e713c20e3763
Sean Dingemans <sxd616@cs.bham.ac.uk>
2018-08-20 14:19:06 +0100

    Created separate testing and development enviroments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true });
+
+//||'mongodb://localhost:27017/TodoApp'
 
 // module.exports = {
 //   mongoose : mongoose;
